e62cc76752ceaf2d2a31933d31cb709a79286892
Syed Aasim <aasimsyed398@gmail.com>
2025-12-03 17:27:10 +0530

    still

--- src/utils/generateOtp.js
+++ src/utils/generateOtp.js
@@ -15,8 +15,7 @@ const generateOtp = async () => {
 const sendEmail = async (email, otp) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
-    port: 587,
-    secure: false,
+    
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
